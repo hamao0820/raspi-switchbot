2aaa100c82320434214d02d385e4e1bfac10a8c4
hamao0820 <hamashun0820@icloud.com>
2025-09-24 10:15:32 +0900

    chiサーバ

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hamao0820/raspi-switchbot
+
+go 1.24.1
+
+require github.com/go-chi/chi v1.5.5
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		panic(err)
+	}
+}
