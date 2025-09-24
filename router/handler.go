7774ab869190b2cf2a6b419e16eb35ecaf95f64a
hamao0820 <hamashun0820@icloud.com>
2025-09-25 07:10:37 +0900

    handlerを追加

---
+++ router/handler.go
@@ -0,0 +1,7 @@
+package router
+
+import "net/http"
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the SwitchBot API"))
+}
--- router/router.go
+++ router/router.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-	"net/http"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -15,9 +14,7 @@ func NewRouter() *chi.Mux {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	r.Get("/", homeHandler)
 
 	return r
 }
